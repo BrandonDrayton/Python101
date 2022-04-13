672ae779bf74761a003b9b3124c83ed2f767280f
Brandon Drayton <brandondrayton@gmail.com>
2022-04-13 10:56:33 -0400

    First thoughts on game project

---
+++ Magika/Magika.py
@@ -0,0 +1,9 @@
+# Intro to command
+
+
+# Game idea is to have wizards fight each other using 8 elements:
+    # Each wizard would combine two elements which then would fire the combined spell towards the other wizard
+    # Certain spells would cancel and/or defeat others (The thinking here is to creat something roughly rock/paper/scissors)
+        # Create elements list which could be referenced
+        # Create combination list?
+        # 
