0aeb02c7e51495eead3c792b57806df80661e88c
Brandon Drayton <brandondrayton@gmail.com>
2022-04-13 22:43:13 -0400

    lots of emojis!

--- Magika/Magika.py
+++ Magika/Magika.py
@@ -1,81 +1,87 @@
 # Fireball > Frostbolt > Lightningbolt > Tornado > Fireball etc...
 
-
+# import pygame
+# import os
 
 import random
+import emoji
+	
 
-possible_actions = ["Fireball", "Frostbolt", "Lightningbolt", "Tornado"]
+# Read.me file or Introduction(Explain the rules)
 
-computer_action = random.choice(possible_actions)
+emoji_list = [" 🔥", " ❄️", " ⚡", " 🌪️" ]
+bot_possible_actions = ["Fireball" +  emoji_list[0], "Frostbolt" + emoji_list[1], "Lightningbolt" + emoji_list[2], "Tornado" + emoji_list[3]]
+
+computer_action = random.choice(bot_possible_actions)
 
 def magika_level_1 (user_action, computer_action):
     if user_action == computer_action:
-        print(f"Both wizards casted {user_action}. The spells canceled each other out! Quick, cast again!!")
+        print(f"Both wizards casted {user_action}. \n\nThe spells canceled each other out! Quick, cast again!!\n")
         return True
-    elif user_action == "Fireball":
-        if computer_action == "Lightningbolt":
-            print("Fireball burned the wizard to death! Onto the next level!")
+    elif user_action == "Fireball" +  emoji_list[0]:
+        if computer_action == "Lightningbolt" + emoji_list[2]:
+            print("Fireball 🔥 burned the wizard to death ☠️! Onto the next level!")
             menu_2
             user_action_2 = menu_2()
-            computer_action_2 = random.choice(possible_actions)
+            computer_action_2 = random.choice(bot_possible_actions)
             magika_level_2(user_action_2, computer_action_2)  
-        elif computer_action == "Frostbolt":
-            print("Fireball melts Frostbolt! You win!")
+        elif computer_action == "Frostbolt" + emoji_list[1]:
+            print("Fireball 🔥 melts Frostbolt ❄️  You win!")
             menu_2
             user_action_2 = menu_2()
-            computer_action_2 = random.choice(possible_actions)
+            computer_action_2 = random.choice(bot_possible_actions)
             magika_level_2(user_action_2, computer_action_2)  
         else:
-            print("Tornado blew away your fire...along with your body. R.I.P")
+            print("Tornado 🌪️ blew away your fire 🔥...along with your body. ☠️\n")
         return False
-    elif user_action == "Frostbolt":
-        if computer_action == "Lightningbolt":
-            print("Frostbolt freezes Lightningbolt! You win!")
+    elif user_action == "Frostbolt" + emoji_list[1]:
+        if computer_action == "Lightningbolt" + emoji_list[2]:
+            print("Frostbolt ❄️  freezes Lightningbolt ⚡ You win!")
         elif computer_action == "Tornado":
-            print("Frostbolt freezes their Tornado!")
+            print("Frostbolt ❄️  freezes their Tornado 🌪️!")
         else:
-            print("Fireball melted your Frostbolt...and then melted you. R.I.P.")
+            print("Fireball 🔥 melted your Frostbolt ❄️   ...and then melted you. ☠️\n")
         return False
-    elif user_action == "Tornado":
-        if computer_action == "Frostbolt":
-            print("Tornado blows away their Frostbolt! You win!")
-        elif computer_action == "Fireball":
-            print("Your tornado snuffed out their fireball...and then their very life.")
+    elif user_action == "Tornado" + emoji_list[3]:
+        if computer_action == "Frostbolt" + emoji_list[1]:
+            print("Tornado 🌪️  blows away their Frostbolt ❄️  You win!")
+        elif computer_action == "Fireball" +  emoji_list[0]:
+            print("Your tornado 🌪️ snuffed out their Fireball 🔥...and then their very life. ☠️")
         else:
-            print("Lightningbolt shocked you into a delusion. You believe you are a beautiful butterfly...you die after jumping from a tree in an attempt to fly. R.I.P.")
+            print("Lightningbolt ⚡ shocked you into a delusion. You believe you are a beautiful butterfly...you die after jumping from a tree in an attempt to fly. ☠️")
         return False
-    elif user_action == "Lightningbolt":
-        if computer_action == "Tornado":
-            print("Your Lightningbolt was faster...he is very dead")
-        elif computer_action == "Fireball":
-            print("Your lightningbolt whizzed past their fireball and killed them.")
+    elif user_action == "Lightningbolt" + emoji_list[2]:
+        if computer_action == "Tornado" + emoji_list[3]:
+            print("Your Lightningbolt ⚡ was faster...he is very dead. ☠️")
+        elif computer_action == "Fireball" +  emoji_list[0]:
+            print("Your lightningbolt ⚡ whizzed past their Fireball 🔥 and killed them. ☠️")
         else:
-            print("Their frostbolt froze you arms off. You quipped 'Tis but a scratch' before perishing.")
+            print("Their frostbolt ❄️  froze you arms off. You quipped 'Tis but a scratch' before perishing. ☠️")
         return False
 def menu():
-    user_action = input("""Choose a spell:
+    user_action = input("""CHOOSE A SPELL:
 
-Press 1 for Fireball
+Press1️⃣  for Fireball 🔥
 
-Press 2 for Frostbolt
+Press2️⃣  for Frostbolt ❄️
 
-Press 3 for Lightningbolt
+Press3️⃣  for Lightningbolt ⚡
 
-Press 4 for Tornado
+Press4️⃣  for Tornado 🌪️
 
     """)
 
     if user_action == "1":
-        user_action = "Fireball"
+        user_action = "Fireball" +  emoji_list[0]
         return user_action
     if user_action == "2":
-        user_action = "Frostbolt"
+        user_action = "Frostbolt" + emoji_list[1]
         return user_action
     if user_action == "3":
-        user_action = "Lightningbolt"
+        user_action = "Lightningbolt" + emoji_list[2]
         return user_action
     if user_action == "4":
-        user_action = "Tornado"
+        user_action = "Tornado" + emoji_list[3]
         return user_action
 def menu_2():
 
@@ -83,12 +89,16 @@ def menu_2():
     
     
     print("""
-    .___  ___. __ .__   __. __   .______    ______       _______.    _______.
-|   \/   ||  ||  \ |  ||  |  |   _  \  /  __  \     /       |   /       |
-|  \  /  ||  ||   \|  ||  |  |  |_)  ||  |  |  |   |   (----`  |   (----`
-|  |\/|  ||  ||  . `  ||  |  |   _  < |  |  |  |    \   \       \   \    
-|  |  |  ||  ||  |\   ||  |  |  |_)  ||  `--'  |.----)   |  .----)   |   
-|__|  |__||__||__| \__||__|  |______/  \______/ |_______/   |_______/    
+
+888b     d8888888888888b    8888888888   888888b.   .d88888b.  .d8888b.  .d8888b. 
+8888b   d8888  888  8888b   888  888     888  "88b d88P" "Y88bd88P  Y88bd88P  Y88b
+88888b.d88888  888  88888b  888  888     888  .88P 888     888Y88b.     Y88b.     
+888Y88888P888  888  888Y88b 888  888     8888888K. 888     888 "Y888b.   "Y888b.  
+888 Y888P 888  888  888 Y88b888  888     888  "Y88b888     888    "Y88b.    "Y88b.
+888  Y8P  888  888  888  Y88888  888     888    888888     888      "888      "888
+888   "   888  888  888   Y8888  888     888   d88PY88b. .d88PY88b  d88PY88b  d88P
+888       8888888888888    Y8888888888   8888888P"  "Y88888P"  "Y8888P"  "Y8888P" 
+                                                                                  
                                                                          
     """)
     user_action_2 = input("""Choose a spell:
@@ -153,30 +163,35 @@ def magika_level_2(user_action_2, computer_action_2):
         return False
 
 
-print(""""
-____    __    ____ _______ __      _______  ______  ______  .___  ___. _______  .___________. ______    .___  ___.     ___      _______ __  __  ___     ___     
-\   \  /  \  /   /|   ____|  |    |   ____|/      |/  __  \ |   \/   ||   ____| |           |/  __  \   |   \/   |    /   \    /  _____|  ||  |/  /    /   \    
- \   \/    \/   / |  |__  |  |    |  |__  |  ,----|  |  |  ||  \  /  ||  |__    `---|  |----|  |  |  |  |  \  /  |   /  ^  \  |  |  __ |  ||  '  /    /  ^  \   
-  \            /  |   __| |  |    |   __| |  |    |  |  |  ||  |\/|  ||   __|       |  |    |  |  |  |  |  |\/|  |  /  /_\  \ |  | |_ ||  ||    <    /  /_\  \  
-   \    /\    /   |  |____|  `----|  |____|  `----|  `--'  ||  |  |  ||  |____      |  |    |  `--'  |  |  |  |  | /  _____  \|  |__| ||  ||  .  \  /  _____  \ 
-    \__/  \__/    |_______|_______|_______|\______|\______/ |__|  |__||_______|     |__|     \______/   |__|  |__|/__/     \__\\______||__||__|\__\/__/     \__\
-                                                                                                                                                                
-""")
+print("""
 
+
+888       8888888888888888      .d8888b.  .d88888b. 888b     d8888888888888      88888888888 .d88888b.       888b     d888       d8888 .d8888b. 8888888888    d8P        d8888
+888   o   888888       888     d88P  Y88bd88P" "Y88b8888b   d8888888                 888    d88P" "Y88b      8888b   d8888      d88888d88P  Y88b  888  888   d8P        d88888
+888  d8b  888888       888     888    888888     88888888b.d88888888                 888    888     888      88888b.d88888     d88P888888    888  888  888  d8P        d88P888
+888 d888b 8888888888   888     888       888     888888Y88888P8888888888             888    888     888      888Y88888P888    d88P 888888         888  888d88K        d88P 888
+888d88888b888888       888     888       888     888888 Y888P 888888                 888    888     888      888 Y888P 888   d88P  888888  88888  888  8888888b      d88P  888
+88888P Y88888888       888     888    888888     888888  Y8P  888888                 888    888     888      888  Y8P  888  d88P   888888    888  888  888  Y88b    d88P   888
+8888P   Y8888888       888     Y88b  d88PY88b. .d88P888   "   888888                 888    Y88b. .d88P      888   "   888 d8888888888Y88b  d88P  888  888   Y88b  d8888888888
+888P     Y888888888888888888888 "Y8888P"  "Y88888P" 888       8888888888888          888     "Y88888P"       888       888d88P     888 "Y8888P888888888888    Y88bd88P     888
+                                                                                                                                                                              
+
+""")
 game_running = True
 while game_running:
     user_action = menu()
 
 
-    print(f"You casted {user_action}")
-    computer_action = random.choice(possible_actions)
-    print(f"The computer casted {computer_action}")
+    print(f"\nYou casted {user_action}\n")
+    computer_action = random.choice(bot_possible_actions)
+    print(f"The computer casted {computer_action}\n")
     game_running = magika_level_1(user_action, computer_action)
     if game_running == False:
-        replay = input("Would you like to play again? ")
+        replay = input("Would you like to play again?\nEnter Yes/No ")
         if replay == "Yes" or replay == "yes":
             game_running = True
         else:
+            print("Thanks for playing. Have a magika day!")
             break
               
 
